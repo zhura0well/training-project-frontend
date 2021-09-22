4b45155ecbc78fdda4f7e230a7cb7a5814a81fa9
Slava <slavonzhuravel@gmail.com>
2021-09-22 18:25:59 +0300

    proxy-changed

--- src/requests.js
+++ src/requests.js
@@ -1,11 +1,9 @@
-const backendUrl = 'https://shop-training-project-backend.herokuapp.com/'
-
 export const getData = async (url) => {
-  const response = await fetch(backendUrl + url)
+  const response = await fetch(url)
   return response.ok ? response.json() : Promise.reject(response)
 }
 export const postData = async (url, data) => {
-  const response = await fetch(backendUrl + url, {
+  const response = await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -16,7 +14,7 @@ export const postData = async (url, data) => {
 }
 
 export const patchData = async (url, data) => {
-  const response = await fetch(backendUrl + url, {
+  const response = await fetch(url, {
     method: 'PATCH',
     headers: {
       'Content-Type': 'application/json',
